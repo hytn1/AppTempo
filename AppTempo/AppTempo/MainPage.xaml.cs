ac68669ed46e3d85e7d99d96297c710d54215abe
Usuario <Usuario@CPD02-M01>
2024-03-21 10:49:33 -0300

    Adicionar arquivos de projeto.

---
+++ AppTempo/AppTempo/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using AppTempo.Models;
+using AppTempo.Services;
+
+namespace AppTempo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Title = "Previsão do Tempo";
+            //define o databinding para os objetos padrão iniciais
+            this.BindingContext = new Tempo();
+        }
+        private async void btnPrevisao_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(cidadeEntry.Text))
+                {
+                    Tempo previsaoDoTempo = await DataService.GetPrevisaoDoTempo(cidadeEntry.Text);
+                    this.BindingContext = previsaoDoTempo;
+                    btnprevisao.Text = "Nova Previsão";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro ", ex.Message, "OK");
+            }
+        
+        }
+    }
+}
---
+++ AppTempo/AppTempo/Models/Tempo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTempo.Models
+{
+    class Tempo
+    {
+        public string Title { get; set; }
+        public string Temperature { get; set; }
+        public string Wind { get; set; }
+        public string Humidity { get; set; }
+        public string Visibility { get; set; }
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+
+        public Tempo()
+        {
+            // Como as Labels se vinculam a estes valores vamos defini-los
+            // como uma string vazia no construtor
+            this.Title = " ";
+            this.Temperature = " ";
+            this.Wind = " ";
+            this.Humidity = " ";
+            this.Visibility = " ";
+            this.Sunrise = " ";
+            this.Sunset = " ";
+        }
+    }
+
+}
